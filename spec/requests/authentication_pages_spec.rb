a967e8e1dc1db3379c46ea1990f1a9d52c1ba545
Will Bentinck <will@bentinck.net>
2012-09-04 22:40:29 +0100

    Complete decoupling exercise

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,8 +6,8 @@ describe "Authentication" do
 	describe "signin page" do
 		before { visit signin_path }
 
-		it { should have_selector('title', text: "Sign in") }
 		it { should have_selector('h1',    text: "Sign in") }
+		it { should have_selector('title', text: "Sign in") }
 	end
 
 	describe "signin" do
@@ -17,21 +17,17 @@ describe "Authentication" do
 			before { click_button "Sign in" }
 
 			it { should have_selector('title', text: "Sign in") }
-			it { should have_selector('div.alert.alert-error', text: "Invalid") }
+			it { should have_error_message('Invalid') }
 
 			describe "after visiting another page" do
 				before { click_link "Home" }
-				it { should_not have_selector('div.alert.alert-error') }
+				it { should_not have_error_message('Invalid') }
 			end
 		end
 
 		describe "with valid info" do
 			let(:user) { FactoryGirl.create(:user) }
-			before do
-			  fill_in "email",    with: user.email
-			  fill_in "password", with: user.password 
-			  click_button "Sign in"
-			end
+			before { valid_signin(user) }
 
 			it { should have_selector('title', text: user.name) }
 			it { should have_link('Profile',  href: user_path(user)) }
